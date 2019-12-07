a1b1fb2c164987f03453c039f44d13922f8c5550
krfisc <krfisc@outlook.com>
2019-12-06 20:44:08 -0600

    hadn't committed in awhile

--- spaceshooter.py
+++ spaceshooter.py
@@ -74,8 +74,18 @@ class Mob(pygame.sprite.Sprite):
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
+    #     self.rot = 0
+    #     self.rot_speed = random.randrange(-8, 8)
+    #     self.last_update = pygame.time.get_ticks()
+    #
+    # def rotate(self):
+    #     now = pygame.time.get_ticks()
+    #     if now - self.last_update > 50:
+    #         self.last_update = now
+
 
     def update(self):
+        # self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if ((self.rect.top > HEIGHT + 10) or (self.rect.left < -25) or (self.rect.right > WIDTH + 20)):
